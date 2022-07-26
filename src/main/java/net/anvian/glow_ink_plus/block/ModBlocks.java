ce053ae789e07bd4f585445171957aca980d549e
anvian <andre.carbajal.vargas@gmail.com>
2022-07-26 11:28:23 -0500

    add recipes and loot_table

--- src/main/java/net/anvian/glow_ink_plus/block/ModBlocks.java
+++ src/main/java/net/anvian/glow_ink_plus/block/ModBlocks.java
@@ -14,6 +14,7 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.world.BlockView;
 
 public class ModBlocks {
+    //wool
     public static final Block GLOW_WHITE_WOOL = registerBlock("glow_white_wool",
             new Block(FabricBlockSettings.of(Material.WOOL, MapColor.WHITE)
                     .strength(0.8f).sounds(BlockSoundGroup.WOOL).luminance(state -> 3)
