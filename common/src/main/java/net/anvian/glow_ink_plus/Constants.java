9496a996de57ca2a56442a411d66555079e52f79
andre-carbajal <carbajalvargasandre@gmail.com>
2024-01-20 13:03:42 -0500

    init multiloader

---
+++ common/src/main/java/net/anvian/glow_ink_plus/Constants.java
@@ -0,0 +1,11 @@
+package net.anvian.glow_ink_plus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "glow_ink_plus";
+	public static final String MOD_NAME = "Glow Ink Plus Mod";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
