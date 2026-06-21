0e9d2a7790db827f6d4e5ab78e2440bba14bfc5d
anviaan <andre.carbajal.vargas@gmail.com>
2026-06-20 19:34:44 -0500

    Implement glowing wool feature for sheep with mixins and accessors

---
+++ common/src/main/java/net/anvian/glow_ink_plus/duck/GlowingSheepAccessor.java
@@ -0,0 +1,6 @@
+package net.anvian.glow_ink_plus.duck;
+
+public interface GlowingSheepAccessor {
+    boolean glowInkPlus$isGlowingWool();
+    void glowInkPlus$setGlowingWool(boolean glowing);
+}
---
+++ common/src/main/java/net/anvian/glow_ink_plus/duck/GlowingSheepRenderStateAccessor.java
@@ -0,0 +1,6 @@
+package net.anvian.glow_ink_plus.duck;
+
+public interface GlowingSheepRenderStateAccessor {
+    boolean glowInkPlus$isGlowingWool();
+    void glowInkPlus$setGlowingWool(boolean glowing);
+}
